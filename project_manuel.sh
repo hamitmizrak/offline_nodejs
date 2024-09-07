06cdbada6969c07546412d41c28406abd625c25c
Hamit Mızrak <hamitmizrak@outlook.com>
2024-09-07 20:46:45 +0300

    project_manuel.sh

--- project_manuel.sh
+++ project_manuel.sh
@@ -5,25 +5,28 @@
 chmod +x project_manuel.sh
 
 # Node js ayağa kaldır
+
+# 1.YOL
+node index.js
+
+# 2.YOL
 # & = anlamı
 # node index.js komutunun çalıştıktan sonra terminali "bloklayarak" (kilitleyerek) diğer komutlara geçişi engellemesidir. 
 # Yani, node index.js çalıştığında terminal bu süreçte bekler ve docker-compose up -d komutunu çalıştırmaz.
-
-# 1.YOL
-node index.js &
-
-# 2.YOL
-#npm start
+# node index.js &
 
 # 3.YOL
+#npm start
+
+# 4.YOL
 # Eğer biz bir script yazarsak run yazmak zorundayız.
 # npm run start
 
-# 4.YOL Nodemon ile ayağa kaldır
+# 5.YOL Nodemon ile ayağa kaldır
 # node monitoring
 # npm run nodemon
 
-# 5.YOL Nodemon ile ayağa kaldır
+# 6.YOL Nodemon ile ayağa kaldır
 # q: terminal çıktısındaki çok detayı gösterme
 #npm run nodemon_q
 
